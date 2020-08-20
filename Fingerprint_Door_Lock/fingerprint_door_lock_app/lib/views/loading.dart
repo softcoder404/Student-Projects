580f5728adb01d4111a46044c16f32409aa92936
softcoder404 <daseeker9@gmail.com>
2020-08-20 07:51:57 +0100

    file modified

--- Fingerprint_Door_Lock/fingerprint_door_lock_app/lib/views/loading.dart
+++ Fingerprint_Door_Lock/fingerprint_door_lock_app/lib/views/loading.dart
@@ -1,38 +1,53 @@
+import 'dart:async';
+
 import 'package:fingerprint_door_lock_app/views/home.dart';
 import 'package:fingerprint_door_lock_app/views/welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:after_layout/after_layout.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
-class _LoadingScreenState extends State<LoadingScreen> with AfterLayoutMixin<LoadingScreen> {
+class _LoadingScreenState extends State<LoadingScreen> {
   Future checkValidity() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool _valid = (prefs.getBool('seen') ?? false);
-
-    if (_valid) {
-      Navigator.of(context).pushReplacement(
-          new MaterialPageRoute(builder: (context) => new HomeScreen()));
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool _isValid = pref.getBool('validity');
+    if (_isValid != null) {
+      if (_isValid) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => HomeScreen(
+                  name: "John Doe !",
+                )));
+      } else {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => LandingScreen()));
+      }
     } else {
-      await prefs.setBool('seen', true);
-      Navigator.of(context).pushReplacement(
-          new MaterialPageRoute(builder: (context) => new LandingScreen()));
+      Timer.periodic(Duration(seconds: 5), (Timer timer) {
+        _isValid = false;
+      });
     }
   }
 
   @override
-  void afterFirstLayout(BuildContext context)=> checkValidity();
-  
+  void initState() {
+    checkValidity();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+        body: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Center(
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.amber,
+        ),
       ),
-    );
+    ));
   }
 }
