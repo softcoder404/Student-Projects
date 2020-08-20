ec040999664638e2fc4aaad6d6a2e44f8bb8dd44
softcoder404 <daseeker9@gmail.com>
2020-08-20 07:51:47 +0100

    file modified

--- Fingerprint_Door_Lock/fingerprint_door_lock_app/lib/views/home.dart
+++ Fingerprint_Door_Lock/fingerprint_door_lock_app/lib/views/home.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
-  static String id = "HomeScreen";
+  final name;
+  HomeScreen({@required this.name});
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future<void> saveData() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+   await pref.setBool('validate', true);
+  }
+
+  @override
+  void initState() {
+    saveData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Center(
+        child: Text('Hom'),
+      ),
+    ),
+    );
   }
 }
