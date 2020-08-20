79dcbfdb8f2332c28016a78a8aaa788cb589e4b6
softcoder404 <daseeker9@gmail.com>
2020-08-20 13:29:09 +0100

    implementing sharedpreferency

--- Fingerprint_Door_Lock/fingerprint_door_lock_app/lib/views/home.dart
+++ Fingerprint_Door_Lock/fingerprint_door_lock_app/lib/views/home.dart
@@ -11,25 +11,24 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   Future<void> saveData() async {
     SharedPreferences pref = await SharedPreferences.getInstance();
-   await pref.setBool('validate', true);
+    await pref.setBool('validate', true);
   }
-
   @override
   void initState() {
-    saveData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    saveData();
     return Scaffold(
       body: Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: Center(
-        child: Text('Hom'),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: Text(widget.name??'John Doe'),
+        ),
       ),
-    ),
     );
   }
 }
