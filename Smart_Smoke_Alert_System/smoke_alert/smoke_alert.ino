294870fa475448cfe72e88422336cfdddf0cda13
softcoder404 <daseeker9@gmail.com>
2020-08-21 17:09:46 +0100

    LiquidCrystal code added

--- Smart_Smoke_Alert_System/smoke_alert/smoke_alert.ino
+++ Smart_Smoke_Alert_System/smoke_alert/smoke_alert.ino
@@ -1,11 +1,18 @@
+#include <LiquidCrystal.h>
 #define smokeSensor A5
 #define buzzer 6
 #define overrideButtonn 5
 #define triggerValue 56
 bool smokeFlag = false;
 uint8_t counter = 0;
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print(" Smoke Detector ");
+  lcd.setCursor(0, 1);
+  lcd.print("And  SMS  Alert ");
   pinMode(smokeSensor, INPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(overrideButtonn, INPUT_PULLUP);
@@ -20,24 +27,43 @@ void setup() {
   delay(1000);
   //Send Ctrl+Z / ESC to denote SMS message is complete
   Serial.write(26);
+  lcd.clear();
 }
 
 void loop() {
   int smokeValue = map(analogRead(smokeSensor), 0 , 1023, 0, 100); //sensor value was mapped to 0 - 100
-  if (smokeValue >= triggerValue) smokeFlag = true;
+  if (smokeValue >= triggerValue) {
+    lcd.clear();
+    smokeFlag = true;
+  }
   else stayAtRest();
   while (smokeFlag) {
     //raise an alarm and wait for 10 secs before sending sms
-    //Serial.println("Smoke Detected Please Take precaution before the count down : " + String(counter));
+    lcd.setCursor(0, 0);
+    lcd.print("Smoke Detected!");
+    lcd.setCursor(0, 1);
+    lcd.print("Take Precaution");
     digitalWrite(buzzer, HIGH);
     if (digitalRead(overrideButtonn) == LOW) {
-      // Serial.println("False Alarm Detected...Keep Calm");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("  False Alarm !");
+      lcd.setCursor(0, 1);
+      lcd.print("Smoke  Override");
+      digitalWrite(buzzer, LOW);
+      delay(2000);
       smokeFlag = false;
     }
     if (counter >= 20) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(" Sending Sms...");
+      lcd.setCursor(0, 1);
+      lcd.print("Please  Wait!!!");
       sendSms();
       delay(1000);
       sendSms2();
+      lcd.clear();
       smokeFlag = false;
     }
     counter ++;
@@ -48,6 +74,10 @@ void loop() {
 
 void stayAtRest() {
   //Serial.println("System At Safe Mode... Keep Calm!");
+  lcd.setCursor(0, 0);
+  lcd.print(" System At Safe");
+  lcd.setCursor(0, 1);
+  lcd.print("     Mode      ");
   digitalWrite(buzzer, LOW);
   smokeFlag = false;
   counter = 0;
