d3cc18d9559c80064f23baf213b40a0668f35528
softcoder404 <daseeker9@gmail.com>
2020-08-21 12:47:08 +0100

    stayAtRest() and sendSms function was written

--- Smart_Smoke_Alert_System/smoke_alert/smoke_alert.ino
+++ Smart_Smoke_Alert_System/smoke_alert/smoke_alert.ino
@@ -2,6 +2,8 @@
 #define buzzer 13
 #define falseAlarmButton 8
 #define triggerValue 56
+bool smokeFlag = false;
+uint8_t counter = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(smokeSensor, INPUT);
@@ -10,7 +12,41 @@ void setup() {
 }
 
 void loop() {
-  int smokeValue = map(analogRead(smokeSensor),0 ,1023, 0, 100); //sensor value was mapped to 0 - 100
-  if(smokeValue >= triggerValue) takeAction();
-  else stayAtRest();  
+  int smokeValue = map(analogRead(smokeSensor), 0 , 1023, 0, 100); //sensor value was mapped to 0 - 100
+  if (smokeValue >= triggerValue) smokeFlag = true;
+  else stayAtRest();
+  while (smokeFlag) {
+    //raise an alarm and wait for 10 secs before sending sms
+    Serial.println("Smoke Detected Please Take precaution before the count down : " + String(counter));
+    digitalWrite(buzzer, HIGH);
+    if (digitalRead(falseAlarmButton) == LOW) {
+      Serial.println("False Alarm Detected...Keep Calm");
+      smokeFlag = false;
+    }
+    if (counter >= 20) {
+      //sendSms
+      Serial.println("Sending sms...");
+      sendSms();
+      delay(3000);
+      //break out of while loop
+      Serial.println("going back to safe mode");
+      delay(1000);
+      smokeFlag = false;
+    }
+    counter ++;
+    delay(500);
   }
+
+}
+
+void stayAtRest() {
+  Serial.println("System At Safe Mode... Keep Calm!");
+  digitalWrite(buzzer, LOW);
+  smokeFlag = false;
+  counter = 0;
+  delay(500);
+}
+
+void sendSms() {
+  //sms code goes here
+}
